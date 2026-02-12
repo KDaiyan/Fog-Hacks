27363938089049266af1238eeb2cf362754c7ab9
Daiyan <daiyankazi08@gmail.com>
2026-02-11 20:44:00 -0800

    added installer, removed contribution (refer to main)

---
+++ installer.py
@@ -0,0 +1,59 @@
+import importlib
+from importlib.metadata import version, PackageNotFoundError
+import platform
+import subprocess
+import sys
+
+
+requirements = {
+    "torch": "2.0.1",
+    "torchvision": "0.15.2",
+    "onnx": "1.14.0",
+    "onnxruntime": "1.15.1",
+    "pycocotools": "2.0.7",
+    "PyYAML": "6.0.1",
+    "scipy": "1.13.0",
+    "onnxslim": "0.1.31",
+    "onnxruntime-gpu": "1.18.0",
+    "gradio": "4.31.5",
+    "opencv-python": "4.9.0.80",
+    "psutil": "5.9.8",
+    "py-cpuinfo": "9.0.0",
+    "huggingface-hub": "0.23.2",
+    "safetensors": "0.4.3",
+}
+
+def is_raspberry_pi():
+    try:
+        with open("/proc/device-tree/model") as f:
+            return "Raspberry Pi" in f.read()
+    except:
+        return False
+
+def run(cmd):
+    print(f"> {cmd}")
+    subprocess.check_call(cmd, shell=True)
+
+pi = is_raspberry_pi()
+
+# Runtime
+print(f"NOTE: Not every dependency is needed to run/train the model\n")
+
+for pkg, req_version in requirements.items():
+    try:
+        installed = version(pkg)
+        status = "OK" if installed >= req_version else f"VERSION MISMATCH (found {installed} but wanted {req_version})"
+    except PackageNotFoundError:
+        status = "NOT INSTALLED"
+
+    print(f"{pkg:20} {status}")
+
+print("\nInstalling dependencies (if any)\n")
+
+if pi:
+    print("Raspberry Pi detected...")
+    run("pip install onnxruntime opencv-python numpy huggingface-hub")
+else:
+    print("Not Raspberry PI")
+    run("pip install -r requirements.txt")
+    run("pip install -e .")
